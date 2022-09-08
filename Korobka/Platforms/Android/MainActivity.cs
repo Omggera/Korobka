e5226f06ade29e1e6d44887eedf56fa38dc8ed40
Omggera <omggera@gmail.com>
2022-09-08 03:54:46 +0300

    Поработал со стилями. Экспериментировал.

--- Korobka/Platforms/Android/MainActivity.cs
+++ Korobka/Platforms/Android/MainActivity.cs
@@ -1,10 +1,16 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace Korobka;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        Window.SetStatusBarColor(Android.Graphics.Color.Rgb(0, 144, 183));
+    }
 }
